85ed936fac8c654da7e09822ed385fb572b6943f
santigc6 <santigc6@users.noreply.github.com>
2018-10-04 19:31:14 +0200

    Update alfabeto.h

--- p1/alfabeto.h
+++ p1/alfabeto.h
@@ -1,1 +1,13 @@
 /**/
+#ifndef ALFABETO_H
+#define ALFABETO_H
+
+typedef struct _Alfabeto Alfabeto;
+
+Alfabeto *crear_alfabeto(int size);
+void destruye_alfabeto(Alfabeto *a);
+int alfabeto_aniade_palabra(Alfabeto *a, char *palabra);
+char *get_palabra_by_index(Alfabeto *a, int index);
+int get_words_number(Alfabeto *a);
+
+#endif
