85150f469f07cf7dfb71f92300dbc52f971977a4
santigc6 <santigc6@users.noreply.github.com>
2018-10-04 19:02:26 +0200

    Update estado.c

--- p1/estado.c
+++ p1/estado.c
@@ -1,6 +1,15 @@
 /**/
+#include <stdio.h>
+#include <string.h>
+#include "estado.h"
 
 struct _Estado{
 	char* nombre;
 	int tipo;
 }
+
+
+Estado *crear_estado(char *name, int type);
+void destruir_estado(Estado *s);
+char *estado_get_name(Estado *s);
+int estado_get_tipo(Estado *s);
