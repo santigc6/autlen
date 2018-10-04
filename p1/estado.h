7fc35b518f7c66276caa6e829dfdd12762079604
santigc6 <santigc6@users.noreply.github.com>
2018-10-04 18:59:47 +0200

    Update estado.h

--- p1/estado.h
+++ p1/estado.h
@@ -2,10 +2,15 @@
 #ifndef ESTADO_H
 #define ESTADO_H
 
+#define INICIAL 2
+#define NORMAL 3
+#define FINAL 4
+
 typedef struct _Estado Estado;
 
-Estado *crear_estado();
+Estado *crear_estado(char *name, int type);
 void destruir_estado(Estado *s);
-
+char *estado_get_name();
+int estado_get_tipo();
 
 #endif
