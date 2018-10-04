b2a050503ea55aef938c6ae8975ff00f4de7d08f
SirWednesday <arnaizaitor@gmail.com>
2018-10-04 19:19:39 +0200

    palabra .c y .h

--- p1/palabra.c
+++ p1/palabra.c
@@ -1,5 +1,47 @@
 /**/
+#include "types.h"
+
+#define MAX_SIZE 256
+
 struct _Palabra {
   char **symbols;
   int size;
 }
+
+char* get_symbol_by_index(Palabra *palabra, int index){
+	
+	if ( palabra && (index >= 0) && (index < (palabra->size)) ){
+		return palabra->symbols[index];
+	}
+}
+
+
+char* get_word_size(Palabra *palabra){
+
+	if( palabra ){
+		return palabra->size;
+	}
+}
+
+int set_symbol_by_index(Palabra *palabra, int index, char* symbol){
+
+	if( palabra && (index >= 0) && (index < (palabra->size)) ){
+		palabra->symbols[index] = symbol;
+
+		return OK;
+	}
+
+	return ERROR;
+}
+
+int set_word_size(Palabra *palabra, int new_size){
+
+	if(palabra && (new_size >= 0) & (new_size < 256) ){
+		palabra->size = new_size;
+		return OK;
+	}
+
+	return ERROR;
+
+
+
--- p1/palabra.h
+++ p1/palabra.h
@@ -1,1 +1,14 @@
 /**/
+
+#ifndef PALABRA_H
+#define PALABRA_H
+
+typedef struct _Palabra Palabra;
+
+char* get_symbol_by_index(Palabra *palabra, int index);
+
+char* get_word_size(Palabra *palabra);
+
+int set_symbol_by_index(Palabra *palabra, int index, char* symbol);
+
+int set_word_size(Palabra *palabra, int new_size);
