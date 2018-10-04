7a13f10f7813ff3481f98d2ba5769e893419751a
santigc6 <santigc6@users.noreply.github.com>
2018-10-04 18:39:00 +0200

    Create afnd.h

---
+++ p1/afnd.h
@@ -0,0 +1,17 @@
+#ifndef AFND_H
+#define AFND_H
+
+AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos);
+void AFNDElimina(AFND * p_afnd);
+void AFNDImprime(FILE * fd, AFND* p_afnd);
+AFND * AFNDInsertaSimbolo(AFND * p_afnd, char * simbolo);
+AFND * AFNDInsertaEstado(AFND * p_afnd, char * nombre, int tipo);
+AFND * AFNDInsertaTransicion(AFND * p_afnd, char * nombre_estado_i, char * nombre_simbolo_entrada, char * nombre_estado_f );
+AFND * AFNDInsertaLetra(AFND * p_afnd, char * letra);
+void AFNDImprimeConjuntoEstadosActual(FILE * fd, AFND * p_afnd);
+void AFNDImprimeCadenaActual(FILE *fd, AFND * p_afnd);
+AFND * AFNDInicializaEstado (AFND * p_afnd);
+void AFNDProcesaEntrada(FILE * fd, AFND * p_afnd);
+void AFNDTransita(AFND * p_afnd);
+
+#endif
