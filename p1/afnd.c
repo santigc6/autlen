14aa684a5ed0bbe6a73eacce5af7803d831bc596
Goldfinger7 <aitor.arnaizdel@estudiante.uam.es>
2018-10-11 19:09:46 +0200

    a guada a alcoholizarse

--- p1/afnd.c
+++ p1/afnd.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
+#include "estado.h"
+
+typedef struct _Transiciones{
+	Estado *inicial;
+	Estado *final;
+	char *trans_symbol;
+} Transicion;
 
 struct _AFND{
 	char *name;
 	Alfabeto *a;
 	Estado* estados;
+	Transicion *transitions;
 }
 
-
-AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos){
-
-
-}
-
+AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos);
 void AFNDElimina(AFND * p_afnd);
 void AFNDImprime(FILE * fd, AFND* p_afnd);
 AFND * AFNDInsertaSimbolo(AFND * p_afnd, char * simbolo);
 AFND * AFNDInsertaEstado(AFND * p_afnd, char * nombre, int tipo);
-AFND * AFNDInsertaTransicion(AFND * p_afnd, char * nombre_estado_i, char * nombre_simbolo_entrada, char * nombre_estado_f );
+
+AFND * AFNDInsertaTransicion(AFND * p_afnd, char * nombre_estado_i, char * nombre_simbolo_entrada, char * nombre_estado_f ){
+	Transicion *t=NULL;
+
+	if(!p_afnd || !nombre_estado_i || !nombre_simbolo_entrada || !nombre_estado_f){
+		return NULL;
+	}
+
+	t=(Transicion *)malloc(sizeof(Transicion));
+	if(!t)
+		return NULL;
+
+	/*t->inicial=*/
+}
+
 AFND * AFNDInsertaLetra(AFND * p_afnd, char * letra);
 void AFNDImprimeConjuntoEstadosActual(FILE * fd, AFND * p_afnd);
 void AFNDImprimeCadenaActual(FILE *fd, AFND * p_afnd);
