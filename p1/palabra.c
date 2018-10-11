61951a73a19b8a9a10fb4ab9941c2afa92f4f4a5
Goldfinger7 <aitor.arnaizdel@estudiante.uam.es>
2018-10-11 18:44:31 +0200

    done week 1

--- p1/palabra.c
+++ p1/palabra.c
@@ -14,62 +14,6 @@ struct _Palabra {
   int current;
 }
 
-
-/********************************************************
-* Name: get_symbol_by_index
-* Description:  returns the symbol stored in the index passed as argument
-********************************************************/
-
-char* get_symbol_by_index(Palabra *palabra, int index){
-	
-	if ( palabra && (index >= 0) && (index < (palabra->size)) ){
-		return palabra->symbols[index];
-	}
-}
-
-/********************************************************
-* Name: get_word_size
-* Description: returns the size of a passed word
-********************************************************/
-
-char* get_word_size(Palabra *palabra){
-
-	if( palabra ){
-		return palabra->current+1;
-	}
-}
-
-/********************************************************
-* Name: set_symbol_by_index
-* Description:  sets the symbol of a certain index in the word 
-********************************************************/
-
-int set_symbol_by_index(Palabra *palabra, int index, char* symbol){
-
-	if( palabra && (index >= 0) && (index < (palabra->size)) ){
-		palabra->symbols[index] = symbol;
-
-		return OK;
-	}
-
-	return ERROR;
-}
-
-/********************************************************
-* Name: set_word_size
-* Description:  sets the size of the passed word
-********************************************************/
-
-int set_word_size(Palabra *palabra, int new_size){
-
-	if(palabra && (new_size >= 0) & (new_size < 256) ){
-		palabra->size = new_size;
-		return OK;
-	}
-
-	return ERROR;
-}
-
 /********************************************************
 * Name: create_word
 * Description:  creates a word with size the passed size
@@ -84,19 +28,22 @@ Palabra* create_word( int size ){
 		return NULL;
 	}
 	 
-	palabra = (char **)malloc(size*(sizeof(char *)));
-
+	palabra = (Palabra *)malloc(sizeof(Palabra));
 	if (!palabra){
+		return NULL;
+	}
+
+	palabra->symbols = (char**)malloc(size*sizeof(char*));
+
+	if(!palabra->symbols){
 		free(palabra);
 		return NULL;
 	}
 
-	for (i = 0; i < size; i++){
-		palabra->symbols[i] = (char*)malloc(MAX_SIZE * sizeof(char));
-	}
-
 	palabra->size = size;
 	palabra->current = 0;
+
+	return palabra;
 }
 
 /********************************************************
@@ -105,18 +52,82 @@ Palabra* create_word( int size ){
 ********************************************************/
 
 void destroy_word(Palabra* palabra){
+	int i = 0;
 
 	if(!palabra){
 		return;
 	}
 
-	int i = 0;
+	if(!palabra->symbols){
+		free(palabra);
+		return;
+	}
 	
-	for(i = 0; i < palabra->size; i++){
-		free(palabra->symbols[i]);
+	for(i = 0; i < palabra->current; i++){
+		if(palabra->symbols[i])
+			free(palabra->symbols[i]);
 	}
 
-	free(palabra->size);
-	free(palabra->current);
+	free(palabra->symbols);
 	free(palabra);
+}
+
+/********************************************************
+* Name: get_symbol_by_index
+* Description:  returns the symbol stored in the index passed as argument
+********************************************************/
+
+char* get_symbol_by_index(Palabra *palabra, int index){
+	if(!palabra || index < 0 || index >= palabra->current){
+		return NULL;
+	}
+
+	return palabra->symbols[index];
+}
+
+/********************************************************
+* Name: get_word_size
+* Description: returns the size of a passed word
+********************************************************/
+
+char* get_word_size(Palabra *palabra){
+	if(!palabra){
+		return NULL;
+	}
+
+	return palabra->current;
+}
+
+/********************************************************
+* Name: set_symbol_by_index
+* Description:  sets the symbol of a certain index in the word 
+********************************************************/
+
+int add_symbol(Palabra *palabra, char* symbol){
+	if(!palabra || !symbol || palabra->current+1 >= palabra->size){
+		return ERROR;
+	}
+
+	palabra->symbols[palabra->current]=(char *)malloc((strlen(symbol)+1)*sizeof(char));
+	if(!palabra->symbols[palabra->current])
+	  return ERROR;
+	  
+	strcpy(palabra->symbols[palabra->current], symbol);
+	palabra->current++;
+
+	return ERROR;
+}
+
+/********************************************************
+* Name: set_word_size
+* Description:  sets the size of the passed word
+********************************************************/
+int set_word_size(Palabra *palabra, int new_size){
+	if(!palabra || new_size <= 0 || palabra->current <= new_size){
+		return ERROR;
+	}
+
+	palabra->size = new_size;
+
+	return ERROR;
 }
--- p1/palabra.h
+++ p1/palabra.h
@@ -4,6 +4,18 @@
 #define PALABRA_H
 
 /********************************************************
+* Name: create_word
+* Description:  creates a word with size the passed size
+********************************************************/
+Palabra* create_word( int size );
+
+/********************************************************
+* Name: destroy_word
+* Description:  destroys the passed word
+********************************************************/
+void destroy_word(Palabra* palabra);
+
+/********************************************************
 * Name: Palabra
 * Description: Palabra data type declaration
 ********************************************************/
@@ -25,24 +37,12 @@ char* get_word_size(Palabra *palabra);
 * Name: set_symbol_by_index
 * Description:  sets the symbol of a certain index in the word 
 ********************************************************/
-int set_symbol_by_index(Palabra *palabra, int index, char* symbol);
+int add_symbol(Palabra *palabra, char* symbol);
 
 /********************************************************
 * Name: set_word_size
 * Description:  sets the size of the passed word
 ********************************************************/
 int set_word_size(Palabra *palabra, int new_size);
-
-/********************************************************
-* Name: create_word
-* Description:  creates a word with size the passed size
-********************************************************/
-Palabra* create_word( int size );
-
-/********************************************************
-* Name: destroy_word
-* Description:  destroys the passed word
-********************************************************/
-void destroy_word(Palabra* palabra);
 
 #endif
